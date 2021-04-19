021c2b56a245c40185c580ac8d5ef198a5eaa3f3
AliAsaf <ali.polat@optdcom.net>
2021-04-19 10:49:48 +0300

    fitness fonksiyonlari yazildi

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -18,7 +18,7 @@ def getAngleBetweenVectors(v1, v2):
     v2_u = unitVector(v2)
     return math.degrees(np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))) % 360
 
-# Verilen iki nokta arasindeki aci bilgisini verir.
+# Verilen iki noktadan cizilen vektoru verir.
 def getVector(x1, y1, x2, y2):
     dX = x2 - x1
     dY = y2 - y1
@@ -27,13 +27,74 @@ def getVector(x1, y1, x2, y2):
 # 9x9 luk alanda rastgele (x,y) koordinat degerleri uretip verir.
 def createSeed(seedUzunlugu):
     createdSeed = []
-    createdSeed.append((0,0))
     for i in range(seedUzunlugu):
-        x = random.randint(0,8)
-        y = random.randint(0,8)
-        createdSeed.append((x,y))
-    createdSeed.append((0,0))
+        direction = random.randint(1,8)
+        createdSeed.append(direction)
     return createdSeed
+
+# Verilen baslangic noktasi ve yon bilgisine gore koordinatlari dondurur.
+def seedToCoordinate(directionSeed, startingPoint):
+    route = []
+    route.append(startingPoint)
+    point = startingPoint
+    for direction in directionSeed:
+        if(direction == 1):
+            point = (point[0], point[1] + 1)
+        elif(direction == 2):
+            point = (point[0] - 1, point[1] + 1)
+        elif(direction == 3):
+            point = (point[0] - 1, point[1])
+        elif(direction == 4):
+            point = (point[0] - 1, point[1] - 1)
+        elif(direction == 5):
+            point = (point[0], point[1] - 1)
+        elif(direction == 6):
+            point = (point[0] + 1, point[1] - 1)
+        elif(direction == 7):
+            point = (point[0] + 1, point[1])
+        elif(direction == 8):
+            point = (point[0] + 1, point[1] + 1)
+        else:
+            print("Unvalid direction")
+            break
+        
+        route.append(point)
+    return route
+            
+# Verilen noktanın istenilen alan icerisinde olup olmadigi bilgisini dondurur.
+def isInTheField(point, fieldWidth, fieldHeight, fieldStartingX, fieldStartingY):
+    if(point[0] < fieldStartingX or point[0] >= fieldStartingX + fieldWidth or point[1] < fieldStartingY 
+       or point[1] >= fieldStartingY + fieldHeight):
+        return False
+    else:
+        return True
+
+# Olusturulan rotadaki koordinatlarin alan icerisinde olup ve birbirlerinden farkli olma sayisi 
+def getDifferentPointsCountInTheField(route):
+    count = 0
+    marked = []
+    for point in route:
+        if(marked.count(point)<1 and isInTheField(point,9,9,0,0)):
+            count += 1
+            marked.append(point)
+    return count
+
+# Bitis noktasi ile hedef nokta arasindaki Manhattan Distance bilgisini verir.
+def getFinalDistancesFromEndPoint(finalPoint, endPoint):
+    return abs(finalPoint[0] - endPoint[0]) + abs(finalPoint[1] - endPoint[1])
+
+# Verilen rotada olusturulan acilarin toplamini verir.
+def getTurningAnglesInRoute(route):
+    angleRes = 0
+    v0 = getVector(route[0][0],route[0][1],route[1][0],route[1][1])
+    
+    for i in range(2,len(route)):
+        v1 = getVector(route[i-1][0],route[i-1][1], route[i][0], route[i][1])
+        angle_ = getAngleBetweenVectors(v0,v1)
+        print("Turning Angle ", i , " : ", angle_)
+        angleRes += angle_
+        v0 = v1
+    return angleRes
 
 if __name__ == '__main__':
     x = createSeed(10)
@@ -42,5 +103,17 @@ if __name__ == '__main__':
     v2 = getVector(1,1,0,1)
     aci = getAngleBetweenVectors(v1,v2)    
     print(x)
-    print(aci)
-        
+    
+    #print(aci)
+    
+    route = seedToCoordinate(x, (0,0)) 
+    for r in route:
+        print(r)
+    
+    diffPointCount = getDifferentPointsCountInTheField(route)
+    print("Different point count in the route : ", diffPointCount)    
+    
+    angleRes = getTurningAnglesInRoute(route)
+    print("Angle addition res : ", angleRes)
+    
+    
